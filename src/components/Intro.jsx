b2ba3a2d2ee9d3cc87f00952a1615bfaedc13d1d
Mert Ayda <48617781+mertayda@users.noreply.github.com>
2022-12-12 04:48:12 +0300

    Add files via upload

---
+++ src/components/Intro.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">
+        Mert Ayda
+      </h1>
+      <p className="text-base md:text-xl mb-3 font-medium">
+        Front-End Developer
+      </p>
+      <p className="text-sm max-w-xl mb-6 font-bold">
+        Enthusiastic and self-taught Frontend Developer skilled in Javascript
+        and React. Passionate about learning new technologies, bringing ideas to
+        life, and willing to work with dedicated teams to build efficient and
+        robust applications suited to user's needs.
+      </p>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ src/data/portfolio.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        title: "TheOne",
+        imageUrl: "./assets/image-1.png",
+        stack: ["React", "Html", "Css"],
+        link: "https://theone-app.netlify.app/"
+    },
+    {
+        title: "Foot Order App",
+        imageUrl: "./assets/image-2.png",
+        stack: ["React,TailwindCss"],
+        link: "https://foodrestaurant1.netlify.app/"
+    },
+    {
+        title: "React Data Analytics App",
+        imageUrl: "./assets/image-3.png",
+        stack: ["React", "TailwindCss"],
+        link: "https://growingwithdata.netlify.app/"
+    },
+    {
+        title: "Defi React App",
+        imageUrl: "./assets/image-4.png",
+        stack: ["React", "TailwindCss"],
+        link: "https://defi-react.netlify.app/"
+    },
+
+
+    {
+        title: "React Crypto App",
+        imageUrl: "./assets/image-5.png",
+        stack: ["React", "TailwindCss"],
+        link: "https://crypto-react-app12.netlify.app/"
+    },
+
+    {
+        title: "React Movie App",
+        imageUrl: "./assets/image-6.png",
+        stack: ["React", "TailwindCss"],
+        link: "https://movies-react-app12.netlify.app/"
+    },
+
+    {
+        title: "Adeptus Mechanicus",
+        imageUrl: "./assets/image-7.png",
+        stack: ["React", "TailwindCss"],
+        link: "#"
+    },
+    {
+        title: "Omnifood",
+        imageUrl: "./assets/image-8.png",
+        stack: ["Javascript", "Html", "Css"],
+        link: "https://omnifood-restaurant-11.netlify.app/"
+    },
+
+    {
+        title: "React Portfolio",
+        imageUrl: "./assets/image-9.png",
+        stack: ["React", "TailwindCss"],
+        link: "https://mrtayda.netlify.app/"
+    },
+
+]
